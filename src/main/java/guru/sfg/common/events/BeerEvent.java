0e65c2658347c063e1cbfb7386f03d842a17d5af
MVishukumar <beingvishu@gmail.com>
2021-10-23 22:48:06 +0530

    Added new services in beer inventory.

---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,19 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = -8522887710499806426L;
+
+    private BeerDto beerDto;
+}
