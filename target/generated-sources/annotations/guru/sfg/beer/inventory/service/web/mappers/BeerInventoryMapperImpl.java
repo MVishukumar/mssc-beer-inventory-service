bfba3d441abcbd446ec5b4101f8756871201d782
MVishukumar <beingvishu@gmail.com>
2021-11-01 22:15:33 +0530

    more commits, more commits, ignore.

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-11-01T15:44:10+0530",
+    date = "2021-11-01T22:13:40+0530",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
 )
 @Component
