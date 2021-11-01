3d73522692f1d22a278adfd7e47e172821a4579a
MVishukumar <beingvishu@gmail.com>
2021-11-01 14:44:40 +0530

    Handle Allocation Requests.

--- src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -9,9 +9,12 @@ import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 public class JmsConfig {
-    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
 
-    // Serialize message content to json using TextMessage
+  public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+  public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+  public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order";
+
+  // Serialize message content to json using TextMessage
     @Bean
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
